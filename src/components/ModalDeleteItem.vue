dd46f4e2e16437b12d1608a7fae6b5a8057303a8
FENIXSHIRO <fenixshiro@gmail.com>
2024-07-26 18:11:51 +0300

    refactor: убрал неиспользуемую переменную

--- src/components/ModalDeleteItem.vue
+++ src/components/ModalDeleteItem.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, defineEmits } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 
 const props = defineProps<{
   itemName: string;
