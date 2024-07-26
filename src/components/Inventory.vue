b381c8a3b81e6a91e02a76fcc9f99980dade2d84
FENIXSHIRO <fenixshiro@gmail.com>
2024-07-26 14:40:25 +0300

    feat: Перетаскивание объектов в ячейках

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="grid-container">
     <div v-for="row in grid" :key="row.id" class="grid-row">
-      <div v-for="cell in row.cells" :key="cell.id" class="grid-cell">
-        {{ cell.id }}
+      <div
+        v-for="cell in row.cells"
+        :key="cell.id"
+        class="grid-cell"
+        @dragover.prevent
+        @drop="drop($event, cell)"
+      >
+        <div
+          v-if="cell.item"
+          class="grid-item"
+          draggable="true"
+          @dragstart="dragStart($event, cell)"
+        >
+          {{ cell.item.name }}
+        </div>
       </div>
     </div>
   </div>
@@ -11,11 +24,16 @@
 <script setup lang="ts">
 import { reactive } from 'vue';
 
-interface Cell {
-  id: number;
+type Item = {
+  name: string;
 }
 
-interface Row {
+type Cell = {
+  id: number;
+  item?: Item;
+}
+
+type Row = {
   id: number;
   cells: Cell[];
 }
@@ -32,6 +50,27 @@ for (let i = 0; i < 5; i++) {
   }
   grid.push(row);
 }
+
+grid[0].cells[0].item = { name: 'Item 1' };
+grid[1].cells[1].item = { name: 'Item 2' };
+
+let draggedItem: { cell: Cell; item: Item } | null = null;
+
+const dragStart = (event: DragEvent, cell: Cell) => {
+  if (cell.item) {
+    draggedItem = { cell, item: cell.item };
+    event.dataTransfer?.setData('text', '');
+  }
+};
+
+const drop = (event: DragEvent, targetCell: Cell) => {
+  if (targetCell.item) return
+  if (draggedItem && draggedItem.item) {
+    targetCell.item = draggedItem.item;
+    draggedItem.cell.item = undefined;
+    draggedItem = null;
+  }
+};
 </script>
 
 <style scoped>
